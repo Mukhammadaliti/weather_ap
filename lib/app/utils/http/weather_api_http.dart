61b8daccf70fd69a0d1a5cfc039388c08bb92756
Mukhammadaliti <mukhammadaliti02@gmail.com>
2023-04-15 14:22:20 +0600

    add somethink

--- lib/app/utils/http/weather_api_http.dart
+++ lib/app/utils/http/weather_api_http.dart
@@ -1,13 +1,16 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 import '../api_keys.dart';
 
-
 class WeatherApiHttp {
-  
-  weatherHttp() {
+  Future<Map<String, dynamic>> weatherHttp(String url) async {
     final client = http.Client();
-    const url =
-        'https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid=597fd8840114926137baeebbb9f68dd5';
+    Uri uri = Uri.parse(url);
+    final response = await client.get(uri);
+    final data = jsonDecode(response.body);
+    return data;
   }
 }
+// 'https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid=597fd8840114926137baeebbb9f68dd5';
